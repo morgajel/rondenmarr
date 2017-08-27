7e56096e7a3f77120972b9fbf9d0903e445b447a
Jesse Morgan <morgajel@gmail.com>
2017-08-27 10:07:04 -0400

    initial import

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+<img style="margin: 0px auto;display:block" src="Ronden Marr Title Logo-smallpatreon.png"/>
+<p>Welcome to Ronden Marr. This is a placeholder until I implement something better.</p>
+<p> In the meantime, please check out the <a href="http://drivethrurpg.com/product/153600/Ronden-Marr-Players-Guide&src=rondenmarr.com" target="_blank" >Ronden Marr Player's Guide</a>.</p>
+</body>
+</html>
